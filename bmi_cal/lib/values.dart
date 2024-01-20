a4d501f535c06f27a5ad96ae998b42a4ca123f6a
Bhavana <tiruveedulabhavana96@gmail.com>
2024-01-21 02:05:27 +0530

    navigate to new page

--- bmi_cal/lib/values.dart
+++ bmi_cal/lib/values.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_cal/age.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Values extends StatefulWidget {
   const Values({super.key});
@@ -14,11 +16,45 @@ class _Values extends State<Values> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      backgroundColor: Colors.amber[200],
       appBar: AppBar(),
-      body: const Center(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        // mainAxisAlignment: MainAxisAlignment.center,
-        child: Text('How tall are you?'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Padding(padding: EdgeInsets.all(8)),
+          const SizedBox(
+            width: 20,
+            height: 80,
+          ),
+          Text(
+            'how tall are you?', //'how tall are you? \n (in cm)' :-to asign new line \n is used.
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 121, 90, 3),
+              fontSize: 33,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '(in cm)',
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 121, 90, 3),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your height here',
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Age(),
+        ],
       ),
     );
   }
