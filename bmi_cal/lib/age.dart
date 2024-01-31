e9a829e924ef5a01106bb9ef5b1f7198662378de
Bhavana <tiruveedulabhavana96@gmail.com>
2024-01-31 20:31:02 +0530

    changed background color

--- bmi_cal/lib/age.dart
+++ bmi_cal/lib/age.dart
@@ -7,47 +7,44 @@ class Age extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Padding(
-      padding: const EdgeInsets.all(18),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "What is your weight ?",
-                style: GoogleFonts.lato(
-                  color: const Color.fromARGB(255, 121, 90, 3),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "What is your weight ?",
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 121, 90, 3),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                '(in kg)',
-                style: GoogleFonts.lato(
-                  color: const Color.fromARGB(255, 121, 90, 3),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            Text(
+              '(in kg)',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 121, 90, 3),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-          const Spacer(),
-          Column(
-            children: [
-              Text(
-                "what's your age?",
-                style: GoogleFonts.lato(
-                  color: const Color.fromARGB(255, 121, 90, 3),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+          ],
+        ),
+        const Spacer(),
+        Column(
+          children: [
+            Text(
+              "what's your age?",
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 121, 90, 3),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- bmi_cal/lib/values.dart
+++ bmi_cal/lib/values.dart
@@ -16,45 +16,46 @@ class _Values extends State<Values> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Colors.amber[200],
       appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Padding(padding: EdgeInsets.all(8)),
-          const SizedBox(
-            width: 20,
-            height: 80,
-          ),
-          Text(
-            'how tall are you?', //'how tall are you? \n (in cm)' :-to asign new line \n is used.
-            style: GoogleFonts.lato(
-              color: const Color.fromARGB(255, 121, 90, 3),
-              fontSize: 33,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            '(in cm)',
-            style: GoogleFonts.lato(
-              color: const Color.fromARGB(255, 121, 90, 3),
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your height here',
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // const SizedBox(
+            //   width: 20,
+            // ),
+            Text(
+              'how tall are you?', //'how tall are you? \n (in cm)' :-to asign new line \n is used.
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 121, 90, 3),
+                fontSize: 33,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          const Age(),
-        ],
+
+            Text(
+              '(in cm)',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 121, 90, 3),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your height here',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Age(),
+          ],
+        ),
       ),
     );
   }
